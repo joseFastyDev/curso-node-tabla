dc0a34d484052f6c7e5c05e40316f94e1a89470c
José Rangel <jose.rangel7030@gmail.com>
2022-09-13 17:59:07 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+
+console.clear();
+
+
+const base = 5;
+
+
+crearArchivo( base )
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch( err => console.log(err));
